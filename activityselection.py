eeafb12d040ea35ee42587ef898e793d7c69b541
Arun Augustine <arunaugustine87@gmail.com>
2012-03-28 10:32:24 -0500

    working question 1 both parts

--- activityselection.py
+++ activityselection.py
@@ -146,7 +146,7 @@ for j in range(1,k+1):   # base case when i = 0 and j > 0
 
 
 # base case when j = 0
-for i in range(1,n+k):
+for i in range(1,n+1):
     q = asp2[i-1][0]
     for m in range(i-1,-1,-1):
         if(isCompatible(activitylist[i],activitylist[m])):
